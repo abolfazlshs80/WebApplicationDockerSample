f9b41bdaf7de9830d0ed82b404dc003edca331af
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-04 08:52:26 +0330

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationDockerSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
